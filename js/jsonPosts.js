7e7404c9aa1e46da53c68691f76c51d441f35f06
flustern <coolgrell@mail.ru>
2023-01-23 18:23:40 +0300

    3 commit

--- js/jsonPosts.js
+++ js/jsonPosts.js
@@ -1,9 +1,26 @@
-[
-    {
-        "albumId": 1,
-        "id": 1,
-        "title": "accusamus beatae ad facilis cum similique qui sunt",
-        "url": "https://via.placeholder.com/600/92c952",
-        "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-      }
-  ]
+const posts = document.querySelector('.products_posts');
+
+var products = JSON.parse(json);
+
+    for (let i = 0; i < products.length; i++) {
+        
+        const div = document.createElement('div');
+        const h3 = document.createElement('h3');
+        const img = document.createElement('img');
+        const p = document.createElement('p');
+
+        h3.innerHTML = products[i].title;
+        img.src = products[i].img;
+        img.style.width = '300px';
+        img.style.height = '300px';
+        img.style.borderRadius = '15px';
+        p.innerHTML = products[i].price;
+        
+
+        
+        div.appendChild(img);
+        div.appendChild(h3);
+        div.appendChild(p);
+
+        posts.appendChild(div);
+    }
