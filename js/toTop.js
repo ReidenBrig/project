347338a193fa122c8be139a8f211632abc14381b
flustern <coolgrell@mail.ru>
2023-01-29 02:06:25 +0300

    commit new

--- js/toTop.js
+++ js/toTop.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let to_top_btn = document.querySelector(".to-up");
 
     window.onscroll = function () {
-        if (window.pageYOffset > 580) {
+        if (window.pageYOffset > 300) {
             to_top_btn.style.display = "block"
         } else {
             to_top_btn.style.display = "none"
